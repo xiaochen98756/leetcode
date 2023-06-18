040d8879537da4f6cc6dc09b62486d9a30a0194c
wangxiaochen <1047800119@qq.com>
2023-06-19 07:03:45 +0800

    update

--- src/main/java/leetcode/Question.java
+++ src/main/java/leetcode/Question.java
@@ -95,16 +95,134 @@ public class Question {
 
     /**
      * 28. 找出字符串中第一个匹配项的下标
+     * haystack = "sadbutsad", needle = "sad" 0
+     * haystack = "leetcode", needle = "leeto" -1
+     *  // System.out.println(strStr("leetcode","leeto"));
+     *  // System.out.println(strStr("badmodebad","mode"));
+     *  //System.out.println(strStr("mississippi","issipi"));
      * @param haystack
      * @param needle
      * @return
      */
-    public int strStr(String haystack, String needle) {
-      return 0;
+    public static int strStr(String haystack, String needle) {
+      if (needle.length()>haystack.length()){
+          return -1;
+      }
+      else if (needle.length()==haystack.length()){
+          if (haystack.equals(needle)){
+              return 0;
+          }else {
+              return -1;
+          }
+      }
+      else {
+          for (int i = 0; i < haystack.length(); i++) {
+              String a=haystack.substring(i,i+1);
+              String b=needle.substring(0,1);
+              if (a.equals(b) && i+needle.length()<=haystack.length()){
+                  String c=haystack.substring(i,i+needle.length());
+                  if (c.equals(needle)){
+                      return i;
+                  }
+              }
+          }
+      }
+      return -1;
     }
+
+    /**
+     * 151. 反转字符串中的单词
+     * @param s
+     * @return
+     */
+    public String reverseWords(String s) {
+        StringBuilder builder=new StringBuilder();
+        String[] lists=s.trim().split("\\s+");
+        for (int i = lists.length - 1; i >= 0; i--) {
+            builder.append(lists[i]).append(" ");
+        }
+        builder.deleteCharAt(builder.length()-1);
+       return builder.toString();
+    }
+
+    /**
+     * 100. 相同的树
+     * @param p
+     * @param q
+     * @return
+     */
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+      if (p==null && q==null){
+          return true;
+      }else if (p!=null && q!=null){
+          if (p.val==q.val){
+              return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+          }else {
+              return false;
+          }
+      }else {
+          return false;
+      }
+    }
+
+    /**
+     * 226. 翻转二叉树
+     * @param root
+     * @return
+     */
+    public TreeNode invertTree(TreeNode root) {
+        if (root==null){
+            return root;
+        }
+        TreeNode left=root.left;
+        TreeNode right=root.right;
+        root.left=right;
+        root.right=left;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+
+    /**
+     * 101. 对称二叉树
+     * @param root
+     * @return
+     */
+    public boolean isSymmetric(TreeNode root) {
+        if (root==null){
+            return true;
+        }
+        if (isSameTree(invertTree(root.left),root.right)){
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 104. 二叉树的最大深度
+     * @param root
+     * @return
+     */
+    public int maxDepth(TreeNode root) {
+       return calDepth(root,0);
+    }
+
+    private int calDepth(TreeNode root, int count) {
+        if (root==null){
+            return count;
+        }else {
+            if (root.left!=null ||root.right!=null){
+                count++;
+                int countleft=calDepth(root.left,0);
+                int countrigtht=calDepth(root.right,0);
+                count=Math.max(countleft,countrigtht);
+            }
+        }
+        return count;
+    }
+
     public static void main(String[] args) {
-          int[] nums=new int[]{2,7,11,15};
-          int target=9;
-        System.out.println(twoSum(nums,target));
+
+
     }
 }
